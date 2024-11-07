1cceafb8285537d4b61cbb956473a5de02bf2400
umutcanbas <umutbas57@gmail.com>
2024-11-07 21:39:46 +0300

    Add and remove favorite

--- src/redux/slice.js
+++ src/redux/slice.js
@@ -1,12 +1,13 @@
 import { createSlice } from '@reduxjs/toolkit'
-
 import { MMKV } from 'react-native-mmkv'
 
 const storage = new MMKV()
 const isLogged = storage.getBoolean('isLogged')
+const storedFavorites = storage.getString('favorites') // Favorileri saklamak için yeni alan
 
 const initialState = {
-  isLogged: isLogged || false
+  isLogged: isLogged || false,
+  favorites: storedFavorites ? JSON.parse(storedFavorites) : [] // Favoriler dizisi
 }
 
 const slice = createSlice({
@@ -15,17 +16,25 @@ const slice = createSlice({
   reducers: {
     login: state => {
       state.isLogged = true
-
       storage.set('isLogged', state.isLogged)
     },
     logout: state => {
       state.isLogged = false
-
       storage.set('isLogged', state.isLogged)
+    },
+    addFavorite: (state, action) => {
+      state.favorites.push(action.payload)
+      storage.set('favorites', JSON.stringify(state.favorites)) // Favorileri güncelle
+    },
+    removeFavorite: (state, action) => {
+      state.favorites = state.favorites.filter(
+        favorite => favorite !== action.payload
+      )
+      storage.set('favorites', JSON.stringify(state.favorites)) // Favorileri güncelle
     }
   }
 })
 
-export const { login, logout } = slice.actions
+export const { login, logout, addFavorite, removeFavorite } = slice.actions
 
 export default slice.reducer
