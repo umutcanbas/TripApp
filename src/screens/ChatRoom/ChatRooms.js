7cc0c9c72fd733edfa720dc9dd9750ed6671ed96
umutcanbas <umutbas57@gmail.com>
2024-11-21 16:59:59 +0300

    Refactor ChatRooms component to include TopMenu and SafeAreaView for improved layout

--- src/screens/ChatRoom/ChatRooms.js
+++ src/screens/ChatRoom/ChatRooms.js
@@ -1,15 +1,23 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import TopMenu from '../../components/TopMenu'
 
-import ChatRoomList from './ChatRoomList';
-
-const ChatRooms = () => {
+const ChatRooms = ({route}) => {
+const title = (route.params.placeName)
 
   return (
-    <ChatRoomList />
+    <SafeAreaView style={styles.container} >
+      <TopMenu title={title} onPressLeft />
+      <Text>ChatRooms</Text>
+    </SafeAreaView>
   )
 }
 
 export default ChatRooms
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#6a5acd',
+  },  
+})
